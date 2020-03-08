4d37bddd556a8f3477d41f09cff6136de38ab580
Grace Shih <vocalise@gmail.com>
2020-03-08 20:09:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    let first_letter = str[0]
+    return first_letter.toUpperCase() + str.slice(1)
+  }
+  
+  static sanitize(str) {
+    return str.replace('/[^A-Za-z0-9\-\'\s]+/g', '');
+  }
+  
+  static titleize(str) {
+    let array = str.split(" ");
+    let new_array = array.map(function(x) {
+      if (['the', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'].includes(x)) {
+        return x;
+      } else {
+        return Formatter.capitalize(x);
+      }
+    })
+    return new_array.join(" ")
+  }
 }
